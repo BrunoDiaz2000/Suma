d3cdbfe8b4a7e2e5fa2192815756f0c342495118
Bruno Godinez <bruno.gomd@gmail.com>
2024-05-06 12:14:17 -0600

    Guardado2

--- MainSuma.java
+++ MainSuma.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 class MainPersona {
    public static void main(String[] args) {
       Scanner op = new Scanner(System.in);
-      System.out.print("Deme la primer variable: ");
+      System.out.print("\nDeme la primer variable: ");
       int X = op.nextInt();
       System.out.print("Deme la segunda variable: ");
       int Y = op.nextInt();
 
       Suma primera = new Suma();
-      primera.x = X;
-      primera.y = Y;
-      
-      primera.mostrarSuma();
+      primera.setX(X);
+      primera.setY(Y);
+
+      System.out.println("\nEl resultado es: "+primera.mostrarSuma());
+
+      System.out.println(primera);
    }
 }
--- Suma.java
+++ Suma.java
@@ -1,9 +1,30 @@
 class Suma {
-   int x,y;
+   private int x,y;
 
    Suma() {}
-
-   public void mostrarSuma() {
-      System.out.println("\nEl resultado es: "+(this.x+this.y));
+   
+   public int mostrarSuma() {
+      return (this.x+this.y);
    }
+
+   public int getX() {
+      return x;
+   }
+
+   public void setX(int x) {
+      this.x = x;
+   }
+
+   public int getY() {
+      return y;
+   }
+
+   public void setY(int y) {
+      this.y = y;
+   }
+
+   @Override
+   public String toString() {
+      return "\nSuma [x=" + x + ", y=" + y + "]\n";
+   }   
 }
